d81aaea81bab68082795e504a5c51de14e849881
g1.kwon <spring_shot@naver.com>
2021-07-05 18:10:37 +0900

    Initial commit

---
+++ src/test/java/basic/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package basic.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
